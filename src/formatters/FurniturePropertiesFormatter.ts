005c60a09f14f9675a0656ba04aa606beafa1337
Mathis HOUIS <mathishouis@gmail.com>
2023-12-31 03:51:52 +0100

    :sparkles: Masks extraction

--- src/formatters/FurniturePropertiesFormatter.ts
+++ src/formatters/FurniturePropertiesFormatter.ts
@@ -13,7 +13,6 @@ export class FurniturePropertiesFormatter {
             colors: [],
             layers: [],
             animations: [],
-            particles: [],
         };
 
         const indexAttributes = indexValue.object.$;
@@ -25,68 +24,84 @@ export class FurniturePropertiesFormatter {
         const directions = logicModel.directions?.[0].direction;
         const visualizations = visualizationValue.visualizationData.graphics[0].visualization;
         const particleSystems = logicValue.objectData.particlesystems?.[0].particlesystem;
+        const masks = logicValue.objectData.mask;
 
-        if (particleSystems) particleSystems.forEach((particleSystem) => {
-            const particleSystemAttributes = particleSystem.$;
-            if (particleSystemAttributes.size === '64') {
-                const particleSystemOutput = {
-                    offsets: {
-                        x: 0,
-                        y: 0,
-                    },
-                    emitters: [],
-                };
+        if (masks) {
+            output['masks'] = [];
+            masks.forEach((mask) => {
+                const maskAttributes = mask.$;
 
-                if (particleSystemAttributes['offset_x']) particleSystemOutput.offsets.x = Number(particleSystemAttributes['offset_x'])
-                if (particleSystemAttributes['offset_y']) particleSystemOutput.offsets.y = Number(particleSystemAttributes['offset_y'])
-                if (particleSystemAttributes['blend']) particleSystemOutput['blend'] = Number(particleSystemAttributes['blend'])
+                output['masks'].push({
+                    id: maskAttributes.type,
+                });
+            });
+        }
 
-                particleSystem.emitter.forEach((emitter) => {
-                    const emitterAttributes = emitter.$;
-                    const emitterOutput = {
-                        id: Number(emitterAttributes.id),
-                        layerId: Number(emitterAttributes.sprite_id),
-                        fuseTime: Number(emitterAttributes.fuse_time),
-                        maxNumParticles: Number(emitterAttributes.max_num_particles),
-                        particlesPerFrame: Number(emitterAttributes.particles_per_frame),
-                        burstPulse: Number(emitterAttributes.burst_pulse),
-                        particles: [],
+
+        if (particleSystems) {
+            output['particles'] = [];
+            particleSystems.forEach((particleSystem) => {
+                const particleSystemAttributes = particleSystem.$;
+                if (particleSystemAttributes.size === '64') {
+                    const particleSystemOutput = {
+                        offsets: {
+                            x: 0,
+                            y: 0,
+                        },
+                        emitters: [],
                     };
 
-                    const simulationAttributes = emitter.simulation[0].$;
-                    if (simulationAttributes['force']) emitterOutput['force'] = Number(simulationAttributes['force']);
-                    if (simulationAttributes['direction']) emitterOutput['direction'] = Number(simulationAttributes['direction']);
-                    if (simulationAttributes['energy']) emitterOutput['energy'] = Number(simulationAttributes['energy']);
-                    if (simulationAttributes['shape']) emitterOutput['shape'] = simulationAttributes['shape'];
-                    if (simulationAttributes['gravity']) emitterOutput['gravity'] = Number(simulationAttributes['gravity']);
-                    if (simulationAttributes['airfriction']) emitterOutput['airFriction'] = Number(simulationAttributes['airfriction']);
+                    if (particleSystemAttributes['offset_x']) particleSystemOutput.offsets.x = Number(particleSystemAttributes['offset_x'])
+                    if (particleSystemAttributes['offset_y']) particleSystemOutput.offsets.y = Number(particleSystemAttributes['offset_y'])
+                    if (particleSystemAttributes['blend']) particleSystemOutput['blend'] = Number(particleSystemAttributes['blend'])
 
-                    const particles = emitter.particles[0].particle;
-
-                    particles.forEach((particle) => {
-                        const particleAttributes = particle.$;
-                        const particleOutput = {
-                            frames: [],
+                    if (particleSystem.emitter) particleSystem.emitter.forEach((emitter) => {
+                        const emitterAttributes = emitter.$;
+                        const emitterOutput = {
+                            id: Number(emitterAttributes.id),
+                            layerId: Number(emitterAttributes.sprite_id),
+                            fuseTime: Number(emitterAttributes.fuse_time),
+                            maxNumParticles: Number(emitterAttributes.max_num_particles),
+                            particlesPerFrame: Number(emitterAttributes.particles_per_frame),
+                            burstPulse: Number(emitterAttributes.burst_pulse),
+                            particles: [],
                         };
 
-                        if (particleAttributes['lifetime']) particleOutput['lifeTime'] = Number(particleAttributes['lifetime']);
-                        if (particleAttributes['fade']) particleOutput['fade'] = particleAttributes['fade'] === 'true';
-                        if (particleAttributes['is_emitter']) particleOutput['emitter'] = particleAttributes['is_emitter'] === 'true';
+                        const simulationAttributes = emitter.simulation[0].$;
+                        if (simulationAttributes['force']) emitterOutput['force'] = Number(simulationAttributes['force']);
+                        if (simulationAttributes['direction']) emitterOutput['direction'] = Number(simulationAttributes['direction']);
+                        if (simulationAttributes['energy']) emitterOutput['energy'] = Number(simulationAttributes['energy']);
+                        if (simulationAttributes['shape']) emitterOutput['shape'] = simulationAttributes['shape'];
+                        if (simulationAttributes['gravity']) emitterOutput['gravity'] = Number(simulationAttributes['gravity']);
+                        if (simulationAttributes['airfriction']) emitterOutput['airFriction'] = Number(simulationAttributes['airfriction']);
 
-                        if (particle.frame) particle.frame.forEach((frame) => {
-                            const frameAttributes = frame.$;
-                            particleOutput.frames.push(frameAttributes.name.replace('_64_', '_'));
+                        const particles = emitter.particles[0].particle;
+
+                        particles.forEach((particle) => {
+                            const particleAttributes = particle.$;
+                            const particleOutput = {
+                                frames: [],
+                            };
+
+                            if (particleAttributes['lifetime']) particleOutput['lifeTime'] = Number(particleAttributes['lifetime']);
+                            if (particleAttributes['fade']) particleOutput['fade'] = particleAttributes['fade'] === 'true';
+                            if (particleAttributes['is_emitter']) particleOutput['emitter'] = particleAttributes['is_emitter'] === 'true';
+
+                            if (particle.frame) particle.frame.forEach((frame) => {
+                                const frameAttributes = frame.$;
+                                particleOutput.frames.push(frameAttributes.name.replace('_64_', '_'));
+                            });
+
+                            emitterOutput.particles.push(particleOutput);
                         });
 
-                        emitterOutput.particles.push(particleOutput);
+                        particleSystemOutput.emitters.push(emitterOutput);
                     });
 
-                    particleSystemOutput.emitters.push(emitterOutput);
-                });
-
-                output.particles.push(particleSystemOutput);
-            }
-        });
+                    output['particles'].push(particleSystemOutput);
+                }
+            });
+        }
 
         if (directions) directions.forEach((direction) => {
            const directionAttributes = direction.$;
@@ -131,7 +146,7 @@ export class FurniturePropertiesFormatter {
                 if (visualization.layers) {
                     const layers = visualization.layers[0].layer;
 
-                    layers.forEach((layer) => {
+                    if (layers) layers.forEach((layer) => {
                         const layerAttributes = layer.$;
                         const layerOutput = {
                             id: Number(layerAttributes.id),
